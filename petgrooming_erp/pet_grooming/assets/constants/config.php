616afbdbea447b16f128c9754a63531ac30ef9e4
richjpex <richard_pecson@dlsu.edu.ph>
2025-10-14 16:59:04 +0800

    fix: hid hardcoded credentials and placed in .env

--- petgrooming_erp/pet_grooming/assets/constants/config.php
+++ petgrooming_erp/pet_grooming/assets/constants/config.php
@@ -1,11 +1,13 @@
 <?php
-// //database settings
+// Database settings
+$servername = $_ENV['DB_HOST'] ?? getenv('DB_HOST');
+$username = $_ENV['DB_USERNAME'] ?? getenv('DB_USERNAME');
+$password = $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD');
+$dbname = $_ENV['DB_NAME'] ?? getenv('DB_NAME');
 
-$servername = "mariadb";
-$username = "root";
-$password = "jK657QqAa0ns";
-$dbname = "pet_grooming";
+// Application settings
+$currency = $_ENV['APP_CURRENCY'] ?? getenv('APP_CURRENCY')
+$timezone = $_ENV['APP_TIMEZONE'] ?? getenv('APP_TIMEZONE');
 
-
-$currency = "INR";
-date_default_timezone_set('Asia/Kolkata');
+date_default_timezone_set($timezone);
+?>
