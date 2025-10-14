036eb382126bcde7aad169516a811a6cbd508e4a
richjpex <richard_pecson@dlsu.edu.ph>
2025-10-14 16:15:45 +0800

    fixed with docker

--- petgrooming_erp/pet_grooming/assets/constants/config.php
+++ petgrooming_erp/pet_grooming/assets/constants/config.php
@@ -1,31 +1,11 @@
 <?php
 // //database settings
 
-
-
-
-$servername = "localhost";
+$servername = "mariadb";
 $username = "root";
-$password = "";
+$password = "jK657QqAa0ns";
 $dbname = "pet_grooming";
 
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "pet_grooming";
-
 $currency = "INR";
 date_default_timezone_set('Asia/Kolkata');
-
-/*
-
-
-//Installation - Include the last slash
-$instalation_dir = "http://localhost/pharmacy";
-
-//Mail settings
-$smtp_host = '';
-$smtp_user = '';
-$smtp_pass = '';
-*/
